660d1735aaeea5ca2643cb943ff97ecd57e54d0d
Kulveer-Kaur <kulveerkaurpctebtech20@gmail.com>
2024-05-25 19:18:03 +0530

    Provide functionality of delete task and edit task

--- todo_app/src/components/Todo.js
+++ todo_app/src/components/Todo.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
-export const Todo = () => {
+export const Todo = ({
+  id,
+  task,
+  isCompleted,
+  isEditing,
+  deleteTodo,
+  editTodo,
+}) => {
   return (
-    <div>Todo</div>
-  )
-}
+    <div className='Todo'>
+      <p>{task}</p>
+      <div>
+        <FontAwesomeIcon icon={faPenToSquare} onClick={() => editTodo(id)} />
+        <FontAwesomeIcon icon={faTrash} onClick={() => deleteTodo(id)} />
+      </div>
+    </div>
+  );
+};
--- todo_app/src/components/TodoWrapper.js
+++ todo_app/src/components/TodoWrapper.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
+import { Todo } from "./Todo";
 import { TodoForm } from "./TodoForm";
 import { v4 as uuidv4 } from "uuid";
+import { EditTodoForm } from "./EditTodoForm";
 uuidv4();
 
 export const TodoWrapper = () => {
@@ -17,10 +19,42 @@ export const TodoWrapper = () => {
     ]);
     console.log(todos);
   };
-
+  const deleteTodo = (id) => setTodos(todos.filter((todo) => todo.id !== id));
+  const editTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo
+      )
+    );
+  };
+  const editTask = (task, id) => {
+    setTodos(todos.map(todo=>todo.id===id?{...todo,task,isEditing:!todo.isEditing}:todo))
+  };
+  /* return (
+    <div className='TodoWrapper'>
+      <h1>Things to do !</h1>
+      <TodoForm addTodo={addTodo} />
+      {todos.map((todo, index) => (
+        <Todo task={todo} key={index} deleteTodo={deleteTodo} />
+      ))}
+    </div>
+  ); */
   return (
     <div className='TodoWrapper'>
       <TodoForm addTodo={addTodo} />
+      {todos.map((e, index) => {
+        /* return <Todo {...e} key={index} deleteTodo={deleteTodo} editTodo={editTodo}/>; */
+        return e.isEditing ? (
+          <EditTodoForm editTodo={editTask} task={e} />
+        ) : (
+          <Todo
+            {...e}
+            key={index}
+            deleteTodo={deleteTodo}
+            editTodo={editTodo}
+          />
+        );
+      })}
     </div>
   );
 };
